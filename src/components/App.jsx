651b8d38744c4a831d074b7864460d65180cd7c3
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-09-19 22:19:03 +0300

    Implemented app logic around ContactForm component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
 import { Component } from 'react';
+import { ContactForm } from './AllComponents';
+
 export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onContactFormSubmit = ({ name, number, id }) => {
+    console.log(name, number, id);
+  };
+
   render() {
     return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitCallback={this.onContactFormSubmit} />
+      </div>
     );
   }
 }
