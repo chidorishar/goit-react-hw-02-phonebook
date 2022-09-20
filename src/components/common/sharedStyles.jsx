805b36986a3710dd9b3481d1f7ad57a18147bb60
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-09-20 21:19:09 +0300

    Added common style for input

--- src/components/common/sharedStyles.jsx
+++ src/components/common/sharedStyles.jsx
@@ -1,17 +1,30 @@
 import { css } from '@emotion/react';
 
-export const activeAccentedButton = p => css`
-  border: ${p.theme.borders.small};
+export const activeAccentedButton = ({ theme }) => css`
+  border: ${theme.borders.small};
 
   cursor: pointer;
-  transition: background-color ${p.theme.transitions.normal},
-    color ${p.theme.transitions.normal};
+  transition: background-color ${theme.transitions.normal},
+    color ${theme.transitions.normal};
 
   &:focus,
   &:hover {
     outline: none;
 
-    background-color: ${p.theme.colors.accent};
-    color: ${p.theme.colors.dark};
+    background-color: ${theme.colors.accent};
+    color: ${theme.colors.dark};
+  }
+`;
+
+export const interactiveInput = ({ theme }) => css`
+  display: block;
+  border-style: solid;
+  outline: none;
+
+  transition: border-color ${theme.transitions.normal};
+
+  &:focus,
+  &:active {
+    border-color: ${theme.colors.accent};
   }
 `;
