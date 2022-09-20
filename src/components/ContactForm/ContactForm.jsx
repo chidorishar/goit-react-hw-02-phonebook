cbb4116471b1b5792fe0f19cdb2644dbbb6c5139
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-09-20 16:55:50 +0300

    Some style elements renaming and styling refactoring

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,12 @@
 import { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { nanoid } from 'nanoid';
-import { Button, Form, Label, Input } from './ContactForm.styled';
+import {
+  AddContactButton,
+  AddContactForm,
+  InputInfoLabel,
+  ContactInput,
+} from './ContactForm.styled';
 
 export class ContactForm extends Component {
   onSubmit = e => {
@@ -19,32 +24,32 @@ export class ContactForm extends Component {
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
-        <Label>
+      <AddContactForm onSubmit={this.onSubmit}>
+        <InputInfoLabel>
           Name
-          <Input
+          <ContactInput
             type="text"
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             autoFocus
-          ></Input>
-        </Label>
-        <Label>
+          ></ContactInput>
+        </InputInfoLabel>
+        <InputInfoLabel>
           Number
-          <Input
+          <ContactInput
             type="tel"
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-          ></Input>
-        </Label>
-        <Button type="submit" cursor="cross">
+          ></ContactInput>
+        </InputInfoLabel>
+        <AddContactButton type="submit" cursor="cross">
           Add contact
-        </Button>
-      </Form>
+        </AddContactButton>
+      </AddContactForm>
     );
   }
 }
--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -1,28 +1,18 @@
 import styled from '@emotion/styled';
+import { activeAccentedButton } from 'components/common/sharedStyles';
 
-export const Button = styled.button`
+export const AddContactButton = styled.button`
+  ${activeAccentedButton};
+
   font-weight: ${p => p.theme.fontWeights.bold};
 
   margin: 0;
   margin-top: ${p => p.theme.space[2]}px;
   padding: ${p => p.theme.space[2]}px;
-  border: ${p => p.theme.borders.small};
   border-radius: ${p => p.theme.radii.big};
-
-  cursor: pointer;
-  transition: background-color ${p => p.theme.transitions.normal},
-    color ${p => p.theme.transitions.normal};
-
-  &:focus,
-  &:hover {
-    outline: none;
-
-    background-color: ${p => p.theme.colors.accent};
-    color: ${p => p.theme.colors.dark};
-  }
 `;
 
-export const Form = styled.form`
+export const AddContactForm = styled.form`
   width: ${p => p.theme.sizes.wide};
   margin-left: ${p => p.theme.space[1]}px;
   padding: ${p => p.theme.space[3]}px;
@@ -30,7 +20,7 @@ export const Form = styled.form`
   border: ${p => p.theme.borders.mediumDashed + p.theme.colors.accentSecondary};
 `;
 
-export const Label = styled.label`
+export const InputInfoLabel = styled.label`
   font-size: ${p => p.theme.fontSizes[2]};
   font-weight: ${p => p.theme.fontWeights.medium};
 
@@ -44,7 +34,7 @@ export const Label = styled.label`
   }
 `;
 
-export const Input = styled.input`
+export const ContactInput = styled.input`
   font-size: ${p => p.theme.fontSizes[1]};
   font-weight: ${p => p.theme.fontWeights.medium};
 
