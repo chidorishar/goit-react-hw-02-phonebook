f13629af9aac26f573f1322de82964462c702e95
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-11-05 16:31:04 +0200

    Made ContactList as component-function

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,57 +1,43 @@
-import { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { Box } from 'components/common/Box/Box.styled';
 import { theme } from 'utils/theme';
 import { ContactInfo, ContactsList, DeleteButton } from './ContactList.styled';
 
-export class ContactList extends Component {
-  state = {
-    contactIdWithHoveredButton: '',
-  };
+export function ContactList(props) {
+  const { contacts, onContactRemoveCallback } = props;
 
-  onDeleteButtonHovered = contactId => {
-    this.setState({ contactIdWithHoveredButton: contactId });
-  };
-
-  render() {
-    return (
-      <ContactsList>
-        {this.props.contacts.map(({ name, number, id }) => (
-          <Box
-            mb={[2]}
-            p={[2]}
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-            border="small"
-            borderRadius="big"
-            borderColor="#00000039"
-            style={{
-              boxShadow:
-                this.state.contactIdWithHoveredButton === id
-                  ? theme.shadows.inputInsetWarning
-                  : theme.shadows.inputInset,
-            }}
-            transition="box-shadow normal"
-            as="li"
-            key={id}
+  return (
+    <ContactsList>
+      {contacts.map(({ name, number, id }) => (
+        <Box
+          mb={[2]}
+          p={[2]}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          border="small"
+          borderRadius="big"
+          borderColor="#00000039"
+          style={{
+            boxShadow: theme.shadows.inputInset,
+          }}
+          transition="box-shadow normal"
+          as="li"
+          key={id}
+        >
+          <ContactInfo>
+            {name}: {number}
+          </ContactInfo>
+          <DeleteButton
+            onClick={() => onContactRemoveCallback(id)}
+            isDelete={true}
           >
-            <ContactInfo>
-              {name}: {number}
-            </ContactInfo>
-            <DeleteButton
-              onClick={() => this.props.onContactRemoveCallback(id)}
-              onMouseEnter={() => this.onDeleteButtonHovered(id)}
-              onMouseOut={() => this.onDeleteButtonHovered('')}
-              isDelete={true}
-            >
-              Delete
-            </DeleteButton>
-          </Box>
-        ))}
-      </ContactsList>
-    );
-  }
+            Delete
+          </DeleteButton>
+        </Box>
+      ))}
+    </ContactsList>
+  );
 }
 
 ContactList.propTypes = {
